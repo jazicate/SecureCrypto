a4cafb64ab424ec588611c22abfd990851d4bef2
Jerome Azicate <azicaj1@unlv.nevada.edu>
2026-03-15 11:20:12 -0700

    Fix CI build directory handling

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jeromeazicate/Desktop/SecureCrypto
-# Build directory: /Users/jeromeazicate/Desktop/SecureCrypto/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/jeromeazicate/Desktop/SecureCrypto/build/securecrypto_tests-b12d07c_include.cmake")
